57af23cffa6fea2f02097de17c00a1705ae81121
baris <bariscankurtkaya@gmail.com>
2021-12-02 17:31:22 +0300

    checkpoint

--- dnClassifier.py
+++ dnClassifier.py
@@ -20,98 +20,94 @@ def averageCalculation(x, img):
     return x
 
 
-def averageCalculationWithNumpy(x,img):
-    averageColumn = np.average(img, axis=1)
-    x = np.average(averageColumn)
-    print(x)
+def averageCalculationWithNumpy(img):
+    averageRow = np.average(img, axis=1)
+    x = np.average(averageRow)
+    #print(x)
     return x
 
+def main(im_listLocation, isNight):
+    #x= 0
+    allSum = 0
+    average = 0
+    allMax = 0
+    maxName = ""
+    allMin = 255
+    minName = ""
+    littleOnes = 0
+    lessOnes = 0
+    bigOnes = 0
+    lessBiggerOnes = 0
+    averagesArray = []
 
+    
+    tic = time.time()
+    im_list = load_dataset(im_listLocation)
+    for l in range(len(im_list)):
+        img = cv2.imread(im_list[l] , cv2.IMREAD_UNCHANGED)
+        #imgAverage = averageCalculation(x,img)
+        imgAverage = averageCalculationWithNumpy(img)
+        averagesArray.append(imgAverage)
+        allSum += imgAverage
+        
+        if(isNight == True):
+            if(imgAverage > allMax):
+                allMax = imgAverage
+                maxName = im_list[l]
+            if(imgAverage > 95):
+                bigOnes = bigOnes + 1
+            if(imgAverage > 90):
+                lessBiggerOnes = lessBiggerOnes + 1
+        else:
+            if(imgAverage < allMin):
+                allMin = imgAverage
+                minName = im_list[l]
+            if(imgAverage < 95):
+                littleOnes = littleOnes + 1
+            if(imgAverage < 100):
+                lessOnes = lessOnes + 1
+    
+    
+    average = allSum/len(im_list)
+
+    print("average: ", average)
+    
+    if(average >= 100):
+        print("DAY")
+        print(l, " , ", minName, " , ", allMin)
+        print("lessOnes: ", lessOnes, " -- littleOnes: ", littleOnes )
+
+    else:
+        print("NIGHT")
+        print(l, " , ", maxName, " , ", allMax)
+        print("lessBiggerOnes: ", lessBiggerOnes, " -- bigOnes: ", bigOnes )
+
+    plt.hist(averagesArray, bins=255, range=[0,255])
+    
+    toc = time.time()
+    print(str(len(im_list)), "Photos in ", str((toc-tic)), "seconds\n")
+    
 ######################### ALGORITHM ##########################
-
-x= 0
-allSum = 0
-average = 0
-allMax = 0
-maxName = ""
-allMin = 255
-minName = ""
-littleOnes = 0
-lessOnes = 0
-bigOnes = 0
-lessBiggerOnes = 0
-averagesArray = []
-
-#### Night ####
-
-im_list = load_dataset("./Dataset/Gece/1/stereo/centre")
 
 #For MacOs
 #im_list = load_dataset("../../../../Volumes/Bariscan/Dataset/Gece/1/stereo/centre")
-
-tic = time.time()
-
-for l in range(len(im_list)):
-    img = cv2.imread(im_list[l] , cv2.IMREAD_UNCHANGED)
-    #imgAverage = averageCalculation(x,img)
-    imgAverage = averageCalculationWithNumpy(x,img)
-    averagesArray.append(imgAverage)
-    allSum += imgAverage
-    if(imgAverage > allMax):
-        allMax = imgAverage
-        maxName = im_list[l]
-        print(l , " , " , maxName , " , " , allMax)
-    if(imgAverage > 100):
-        bigOnes = bigOnes + 1
-    if(imgAverage > 95):
-        lessBiggerOnes = lessBiggerOnes + 1
-    print("lessBiggerOnes: ", lessBiggerOnes, " -- bigOnes: ", bigOnes )
+#im_list = load_dataset("../../../../Volumes/Bariscan/Dataset/sample/stereo/centre")
+#im_list = load_dataset("../../../../Volumes/Bariscan/Dataset/gunduz1/Centre")
 
 
-"""
-#### Day ####
+im_list_array = ["./Dataset/Gece/1/stereo/centre", "./Dataset/Gece3/1/stereo/centre", "./Dataset/gunduz1/centre", "./Dataset/sample/stereo/centre", "./Dataset/gunduz2/1/stereo/centre", "./Dataset/gunduz3/1/stereo/centre"]
 
-#im_list = load_dataset("./Dataset/gunduz1/centre")
+for i in range(len(im_list_array)):
+    if "Gece" in im_list_array[i]:
+        isNight = True
+    else:
+        isNight = False
 
-#im_list = load_dataset("./Dataset/sample/stereo/centre")
-
-#For my MacOs
-im_list = load_dataset("../../../../Volumes/Bariscan/Dataset/sample/stereo/centre")
-im_list = load_dataset("../../../../Volumes/Bariscan/Dataset/gunduz1/Centre")
+    main(im_list_array[i], isNight)
 
 
 
-for l in range(len(im_list)):
-    img = cv2.imread(im_list[l] , cv2.IMREAD_UNCHANGED)
-    #imgAverage = averageCalculation(x,img)
-    imgAverage = averageCalculationWithNumpy(x,img)
-    allSum += imgAverage
-    if(imgAverage < allMin):
-        allMin = imgAverage
-        minName = im_list[l]
-        print(l , " , " , minName , " , " , allMin)
-    if(imgAverage < 100):
-        littleOnes = littleOnes + 1
-    if(imgAverage < 105):
-        lessOnes = lessOnes + 1
-    print("lessOnes: ", lessOnes, " -- littleOnes: ", littleOnes )
-"""
 
-toc = time.time()
-average = allSum/len(im_list)
-
-print("average: ", average)
-
-if(average >= 100):
-    print("day")
-    print(l, " , ", minName, " , ", allMin)
-else:
-    print("night")
-    print(l, " , ", maxName, " , ", allMax)
-
-print(str(len(im_list)), "Photos in ", str((toc-tic)), "seconds")
-
-plt.hist(averagesArray, bins=100)
 plt.show()
 
 
