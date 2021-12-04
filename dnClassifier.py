ea80a3cc16ab14f00a7bb761fefd48e6fe7851ab
baris <bariscankurtkaya@gmail.com>
2021-12-04 17:49:53 +0300

    new enhancements added

--- dnClassifier.py
+++ dnClassifier.py
@@ -1,6 +1,4 @@
-import os
 import cv2
-import matplotlib.image as mpimg
 import matplotlib.pyplot as plt
 import glob
 import numpy as np
@@ -26,7 +24,22 @@ def averageCalculationWithNumpy(img):
     #print(x)
     return x
 
-def mainAlgorithm(im_listLocation, isNight, averagesArray):
+
+def totalImageCalculator(im_list, imgArray):
+    tic = time.time()
+    
+    for l in range(len(im_list)):
+        img = cv2.imread(im_list[l] , cv2.IMREAD_UNCHANGED)
+        imgArray = np.add(imgArray,img)
+        
+    toc = time.time()
+    print(str(len(im_list)), "Photos in ", str((toc-tic)), "seconds\n")
+    return imgArray;
+
+
+
+
+def thresholdTest(im_list, isNight, averagesArray):
     #x= 0
     allSum = 0
     average = 0
@@ -36,29 +49,32 @@ def mainAlgorithm(im_listLocation, isNight, averagesArray):
     minName = ""
     dayErrorCount = 0    
     nightErrorCount = 0
-
-    
+        
     tic = time.time()
-    im_list = load_dataset(im_listLocation)
+
     for l in range(len(im_list)):
         img = cv2.imread(im_list[l] , cv2.IMREAD_UNCHANGED)
+        
+        #img = img[0:201,500:900]
         #imgAverage = averageCalculation(x,img)
+        
         imgAverage = averageCalculationWithNumpy(img)
         averagesArray.append(imgAverage)
         allSum += imgAverage
+        
         
         if(isNight == True):
             if(imgAverage > allMax):
                 allMax = imgAverage
                 maxName = im_list[l]
-            if(imgAverage > 95):
+            if(imgAverage > 75):
                 nightErrorCount = nightErrorCount + 1
 
         else:
             if(imgAverage < allMin):
                 allMin = imgAverage
                 minName = im_list[l]
-            if(imgAverage < 95):
+            if(imgAverage < 75):
                 dayErrorCount = dayErrorCount + 1
 
     
@@ -76,10 +92,27 @@ def mainAlgorithm(im_listLocation, isNight, averagesArray):
         print("NIGHT")
         print(l, " , ", maxName, " , ", allMax)
         print("nightErrorCount: ", nightErrorCount)
-
     toc = time.time()
     print(str(len(im_list)), "Photos in ", str((toc-tic)), "seconds\n")
-    return averagesArray
+    return averagesArray;
+    
+
+def histogramGenerator(nightAveragesArray, dayAveragesArray):
+    plt.hist(nightAveragesArray, bins=255, range=[0,255])
+    plt.hist(dayAveragesArray, bins=255, range=[0,255])
+
+    plt.show()
+    
+
+def averageImgDisplay(nightImageArray, dayImageArray):
+    nightImageAveragesArray = np.true_divide(nightImageArray, nightImageCount)
+    dayImageAveragesArray = np.true_divide(dayImageArray, dayImageCount)
+
+    cv2.imshow("night",nightImageAveragesArray)
+    cv2.imshow("day",dayImageAveragesArray)
+    
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
     
 ######################### ALGORITHM ##########################
 
@@ -90,25 +123,46 @@ def mainAlgorithm(im_listLocation, isNight, averagesArray):
 nightAveragesArray = []
 dayAveragesArray = []
 
+nightImageArray = np.array([0]) #It will keep all photographs average pixel by pixel
+dayImageArray = np.array([0])
+
+nightImageCount = 0
+dayImageCount = 0
+
 im_list_array = ["./Dataset/Gece/1/stereo/centre", "./Dataset/Gece3/1/stereo/centre", "./Dataset/Gece4/1/stereo/centre", "./Dataset/gunduz2/1/stereo/centre", "./Dataset/gunduz3/1/stereo/centre", "./Dataset/gunduz4/1/stereo/centre"]
 
 
+isDisplay = True
+
 for i in range(len(im_list_array)):
+    im_list = load_dataset(im_list_array[i])
+    
     if "Gece" in im_list_array[i]:
         isNight = True
-        averagesArray = mainAlgorithm(im_list_array[i], isNight, nightAveragesArray)
-        nightAveragesArray += averagesArray
+        nightImageCount += len(im_list)
+        
+        if not isDisplay:
+            averagesArray = thresholdTest(im_list, isNight, nightAveragesArray)
+            nightAveragesArray += averagesArray
+            
+        else:
+            nightImageArray = totalImageCalculator(im_list, nightImageArray)
+            
     else:
         isNight = False
-        averagesArray = mainAlgorithm(im_list_array[i], isNight, dayAveragesArray)
-        dayAveragesArray += averagesArray
+        dayImageCount += len(im_list)
         
-plt.hist(nightAveragesArray, bins=255, range=[0,255])
-plt.hist(dayAveragesArray, bins=255, range=[0,255])
+        if not isDisplay:
+            averagesArray = thresholdTest(im_list, isNight, dayAveragesArray)
+            dayAveragesArray += averagesArray
+            
+        else:
+            dayImageArray = totalImageCalculator(im_list, dayImageArray)
 
+        
+        
 
-plt.show()
-
-
-#cv2.waitKey(0)
-#cv2.destroyAllWindows()
+if not isDisplay:
+    histogramGenerator(nightAveragesArray, dayAveragesArray)
+else:
+    averageImgDisplay(nightImageArray, nightImageCount, dayImageArray, dayImageCount)
