9e21fb3910ed50191440c7895f330885f2a5a212
bkurtkaya <bariscankurtkaya@gmail.com>
2022-05-04 05:31:16 +0300

    bug

--- src/createDatasetTxt.py
+++ src/createDatasetTxt.py
@@ -5,25 +5,27 @@ import glob
 file_direction = "../Dataset/gece1.1/*.xml"
 xml_files = glob.glob("*.xml")
 png_files = glob.glob("*.png")
+txt_files = glob.glob("*.txt")
 
 def create_file_for_train(files, name):
     with open(name, 'w') as f:
         for i in range(len(files)):
-            f.write(files[i] + "\n")
+            print(files[i])
+            f.write("data/gece1.1/" + files[i] + "\n")
 
 
 def delete_xml_files(xml_files):
     for i in range(len(xml_files)):
-        os.system("rm " + xml_files[i])
+        os.system("rm " + xlm_files[i])
 
 if __name__ == "__main__":
     #print(xml_files)
     #delete_xml_files(xml_files)
     
-    train_count = len(png_files) * 0.8
+    train_count = len(txt_files) * 0.8
     train_count = round(train_count)
-    create_file_for_train(png_files[:train_count], "train.txt")
-    create_file_for_train(png_files[train_count:], "test.txt")
+    create_file_for_train(txt_files[:train_count], "train.txt")
+    create_file_for_train(txt_files[train_count:], "test.txt")
 
     print("Success" , train_count)
     
